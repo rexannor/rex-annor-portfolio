3592164d8bce5649af7b5eb2911a1af826e5a161
rexannor <rexannor@gmail.com>
2025-06-30 20:02:45 -0400

    Update tailwin.config.js

--- tailwin.config.js
+++ tailwin.config.js
@@ -5,7 +5,16 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark-primary': 'hsl(0, 0%, 6.7%)',
+        'dark-secondary': 'hsl(0, 0%, 13.3%)',
+        'light-bg': 'hsl(0, 0%, 97.6%)',
+        'text-primary': 'hsl(0, 0%, 20%)',
+        'text-secondary': 'hsl(0, 0%, 40%)',
+        'nav-light': 'hsl(0, 0%, 80%)',
+      },
+    },
   },
   plugins: [],
 }
